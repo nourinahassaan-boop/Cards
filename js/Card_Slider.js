037980275eb331388e7754a868797a16abf3901c
Nourina <nourinahassaan@gmail.com>
2025-11-06 20:33:48 +0500

    Card_folder_upload
    Multiple cards

---
+++ js/Card_Slider.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper"),
+ carousel = document.querySelector(".carousel"),
+ img = document.querySelectorAll("img"),
+ buttons = document.querySelectorAll(".button");
+
+ let imageIndex = 1,
+ intervalId;
+
+ const autoSlide = () => {
+    intervalId = setInterval (() => slideImage (++imageIndex) , 2000);
+ };
+ autoSlide();
+
+ const slideImage = () => {
+    imageIndex = imageIndex ===img.length ? 0 : imageIndex < 0 ? img.length -1 : imageIndex;
+    carousel.style.transform = `translate(-${imageIndex * 100}%)`;
+ }
+ const updateClick = (e) => {
+    clearInterval(intervalId);
+    imageIndex += e.target.id === "next" ? 1 : -1;
+    slideImage(imageIndex);
+    autoSlide();
+ }
+buttons.forEach(button => button.addEventListener("click" ,updateClick))
+ wrapper.addEventListener("mouseover" , () => clearInterval(intervalId));
+ wrapper.addEventListener("mouseleave" , autoSlide);
